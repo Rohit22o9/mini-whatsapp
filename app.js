481b7994d919ffe18c03efc95a10a2b80cf9cda7
Rohit22o9 <rratnaparkhi3@gmail.com>
2025-07-28 14:10:40 +0530

    Add start script

--- app.js
+++ app.js
@@ -186,6 +186,7 @@ for (let iface of Object.values(networkInterfaces)) {
     }
 }
 
-app.listen(3000, localIp, () => {
-    console.log(`Server running on http://${localIp}:3000`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
